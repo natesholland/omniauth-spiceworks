370c85d79126b71ea354699d8a3a3425ddcfa412
Fraser Mince <frasermince@gmail.com>
2015-09-08 16:12:19 -0500

    Change default scope to be public.

--- lib/omniauth/strategies/spiceworks.rb
+++ lib/omniauth/strategies/spiceworks.rb
@@ -2,18 +2,18 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class Spiceworks < OmniAuth::Strategies::OAuth2
-      DEFAULT_SCOPE = 'default'
+      DEFAULT_SCOPE = 'public'
       option :name, 'spiceworks'
       option :authorize_options, [:scope]
       option :client_options, {
         site: 'https://accounts.spiceworks.com'
       }
-      uid{raw_info['spiceworks_id']}
+      uid { raw_info['spiceworks_id'] }
       info do
         {
           first_name: raw_info['first_name'],
           last_name: raw_info['last_name'],
-          name: raw_info['first_name'],
+          name: raw_info['first_name'].present? ? raw_info['first_name'] : 'Anonymous',
           email: raw_info['email']
         }
       end
@@ -24,20 +24,16 @@ module OmniAuth
       end
 
       def authorize_params
-        puts "***SESSION #{@env}"
-        puts "***SESSION #{session}"
-        puts "***SUPER #{super}"
         super.tap do |params|
-          puts "***PARAMS #{params}"
           params[:scope] ||= DEFAULT_SCOPE
         end
       end
 
       private
+
       def raw_info
         @raw_info ||= access_token.get('http://www.account.spiceworks.com/api/public/v1/users/user').parsed['user'] || {}
       end
-
     end
   end
 end
